aab275419706a1271e1d36fb28bab9aed6f29fe0
jameslai <hoktsungjamesl2027@student.cis.edu.hk>
2024-08-28 09:46:41 +0800

    I coded

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your age, please:");
+        int age = scanner.nextInt();
+        int dateAge = (age / 2) + 7;
+        System.out.println(age+ " year olds should date somebody who is at least "+ dateAge + " years old");
+        scanner.close();
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.Scanner;
+
 public class Pages {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your age, please:");
+        int age = scanner.nextInt();
+        int dateAge = (100 - age);
+        System.out.println("" + age + " you should at least read: " + dateAge + " pages");
+        scanner.close();
     }
 }
